8cb33ede4d070b365ed9ff43eebc629afe5a376d
Srija Ragula <srijareddy1808@gmail.com>
2025-08-01 11:28:29 +0530

    Update EvenOdd.java

--- JavaAssignments/src/allassignments/EvenOdd.java
+++ JavaAssignments/src/allassignments/EvenOdd.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class EvenOdd {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//kjhjhgbhjb
 		int a=6;
 		if(a%2==0){
 			System.out.println("even");
