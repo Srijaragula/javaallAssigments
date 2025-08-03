8b2b1fa43bf40142c1a3401019ec93ac0d130712
Srija Ragula <srijareddy1808@gmail.com>
2025-08-03 21:38:24 +0530

    Update DriverInsurance.java

--- JavaAssignments/src/allassignments/DriverInsurance.java
+++ JavaAssignments/src/allassignments/DriverInsurance.java
@@ -8,7 +8,7 @@ public class DriverInsurance {
 		boolean married=true;// ture ot false
 		int age=31;
 		if(married) {
-			System.out.println("driver is married");
+			System.out.println("driver is married");//jhbjhb
 		}
 		else if(gender.equalsIgnoreCase("male") && age>30) {
 			System.out.println("the driver is insured and unmarried male above age 30.");
