21919c26c8e399e3fd29da50decbd0cbbe714736
Srija Ragula <srijareddy1808@gmail.com>
2025-08-06 12:51:41 +0530

    Update InsertMove.java

--- JavaAssignments/src/allassignments/InsertMove.java
+++ JavaAssignments/src/allassignments/InsertMove.java
@@ -7,7 +7,7 @@ public class InsertMove {
 	public static void main(String[] args) {
 	int[] r= {20,27,89,40};
 	int[] arr=new int[r.length+1];
-	r[0]=18;
+	r[0]=18;//yjyy
 	
 	for(int i=0;i<r.length;i++) {
 		arr[i+1]=r[i];
