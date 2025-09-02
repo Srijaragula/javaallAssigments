5077a2ecf51ee356cb06b1be642cac16d0b1e8e0
Srija Ragula <srijareddy1808@gmail.com>
2025-09-02 12:16:56 +0530

    Update IfElse.java

--- JavaAssignments/src/allassignments/IfElse.java
+++ JavaAssignments/src/allassignments/IfElse.java
@@ -4,7 +4,7 @@ public class IfElse {
 
 	public static void main(String[] args) {
 		int bal_amt=5000,withdw_amt=6000;
-		if(bal_amt>=withdw_amt)
+		if(bal_amt>=withdw_amt)/hjgjhg
 		{
 			bal_amt-=withdw_amt;
 			System.out.println("balance:"+bal_amt);
