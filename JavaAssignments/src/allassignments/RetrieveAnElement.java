5f4968827d58e49fd9a80194a24ab8a98e9babfe
Srija Ragula <srijareddy1808@gmail.com>
2025-09-09 00:20:13 +0530

    Update RetrieveAnElement.java

--- JavaAssignments/src/allassignments/RetrieveAnElement.java
+++ JavaAssignments/src/allassignments/RetrieveAnElement.java
@@ -5,7 +5,7 @@ public class RetrieveAnElement {
 	public static void main(String[] args) {
 		 int[] numbers = { 10, 20, 30, 40, 50 };
 
-	        // Access elements via their indices (zero-based)
+	        // Access elements via their indices (zero-based)//jj
 	        int firstElement = numbers[0];    // 10
 	        int thirdElement = numbers[2];    // 30
 
