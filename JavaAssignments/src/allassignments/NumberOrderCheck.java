690c37b6921eae4d95e48ce5a913aea3ecc240c9
Srija Ragula <srijareddy1808@gmail.com>
2025-09-09 00:18:28 +0530

    Update NumberOrderCheck.java

--- JavaAssignments/src/allassignments/NumberOrderCheck.java
+++ JavaAssignments/src/allassignments/NumberOrderCheck.java
@@ -7,7 +7,7 @@ public class NumberOrderCheck {
 		int num2=80;
 		int num3=70;
 		
-       if(num1<num2 && num2<num3) {
+       if(num1<num2 && num2<num3) {//jnj
     	   System.out.println("numbers are in increasing order");
        }else if(num1>num2 && num2>num3) {
     	   System.out.println("numbers are in decreasing order");
