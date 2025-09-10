bc9b3b6761a7de22e414193db37c2965fff96cfe
Srija Ragula <srijareddy1808@gmail.com>
2025-09-10 23:06:41 +0530

    Update Employee.java

--- JavaAssignments/src/allassignments/Employee.java
+++ JavaAssignments/src/allassignments/Employee.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class Employee {
 
-	static String companyname="LTTS";//ltts
+	static String companyname="LTTS";//ltt
 	public static void main(String[] args) {
 		System.out.println("companyname "+companyname);
 		System.out.println("name: "+Employee.companyname);
