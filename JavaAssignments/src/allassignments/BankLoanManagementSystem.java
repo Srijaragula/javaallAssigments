bc452971d28c3d4cbaa5379a2b3548f604776b27
Srija Ragula <srijareddy1808@gmail.com>
2025-09-10 23:03:27 +0530

    Update BankLoanManagementSystem.java

--- JavaAssignments/src/allassignments/BankLoanManagementSystem.java
+++ JavaAssignments/src/allassignments/BankLoanManagementSystem.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class BankLoanManagementSystem {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);//jhjh
+		Scanner sc=new Scanner(System.in);//jhj
 		
 		String registeredUsername="";
 		String registeredPassword="";
