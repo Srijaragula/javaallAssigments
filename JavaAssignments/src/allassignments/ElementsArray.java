1962d4ed5df64f9ffbbcff931d27d1a16a3779c2
Srija Ragula <srijareddy1808@gmail.com>
2025-09-11 21:52:53 +0530

    Update ElementsArray.java

--- JavaAssignments/src/allassignments/ElementsArray.java
+++ JavaAssignments/src/allassignments/ElementsArray.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class ElementsArray {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//ygyg
 		int[] r= {23,25,68,49,50};//uyh
 		for(int i=0;i<r.length;i++) {
 			System.out.println(r[i]);
