e59a2c84ee305b81552589d3a0247f5c7d06e37c
Srija Ragula <srijareddy1808@gmail.com>
2025-09-27 22:16:30 +0530

    Update Div5and2.java

--- JavaAssignments/src/allassignments/Div5and2.java
+++ JavaAssignments/src/allassignments/Div5and2.java
@@ -4,7 +4,7 @@ public class Div5and2 {
 
 	public static void main(String[] args) {
 		int s=8;
-		if(s%5==0 && s%11==0) {//kmkthm
+		if(s%5==0 && s%11==0) {//km
 			System.out.println("number divisibe by both 5 and 11");//jhjh
 		}
 		else {//kj
