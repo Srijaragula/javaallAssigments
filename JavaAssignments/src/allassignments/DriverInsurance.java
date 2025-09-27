7f68daf6004a8f3037dfd6d304a20488215c1ba5
Srija Ragula <srijareddy1808@gmail.com>
2025-09-27 22:16:53 +0530

    Update DriverInsurance.java

--- JavaAssignments/src/allassignments/DriverInsurance.java
+++ JavaAssignments/src/allassignments/DriverInsurance.java
@@ -4,7 +4,7 @@ public class DriverInsurance {
 
 	public static void main(String[] args) {
 		
-		String gender="Female";//"male" or "female"//uh
+		String gender="Female";//"male" or "female"//uh 
 		boolean married=true;// ture ot false//t or f
 		int age=31;//uhujh
 		if(married) {
