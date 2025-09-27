084d947e559a1c985c3d1c9e335aeea41171ad15
Srija Ragula <srijareddy1808@gmail.com>
2025-09-27 22:14:02 +0530

    Update Average2.java

--- JavaAssignments/src/allassignments/Average2.java
+++ JavaAssignments/src/allassignments/Average2.java
@@ -6,7 +6,7 @@ public class Average2 {
 		int a=1;
 		int b=2;
 		int c=3;
-		int d=4;//hg
+		int d=4;//h
 		int e=5;
 		float num=(a+b+c+d+e)/5;
 		System.out.println("Average of 5 numbers :"+num);
