61bc0283d29f73176dc2215921953a2daff1c236
Srija Ragula <srijareddy1808@gmail.com>
2025-09-27 22:13:17 +0530

    Update Arithematic11.java

--- JavaAssignments/src/allassignments/Arithematic11.java
+++ JavaAssignments/src/allassignments/Arithematic11.java
@@ -4,7 +4,7 @@ public class Arithematic11 {
 
 	public static void main(String[] args) {//hb
 		int a=2;
-		int b=4;//fg
+		int b=4;//fg 
 		System.out.println("Additon of two numbers is:"+(a+b));
 		System.out.println("Substraction of two numbers is:"+(a-b));
 		System.out.println("Division of two numbers is:"+(a/b));
