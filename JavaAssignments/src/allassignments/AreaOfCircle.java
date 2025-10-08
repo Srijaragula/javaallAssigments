78591c132dd85eb7c9d99fb347cff098a9bb12cb
Srija Ragula <srijareddy1808@gmail.com>
2025-10-08 14:48:23 +0530

    Update AreaOfCircle.java

--- JavaAssignments/src/allassignments/AreaOfCircle.java
+++ JavaAssignments/src/allassignments/AreaOfCircle.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class AreaOfCircle {
 //bjj
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		int r=4;
 		double area=3.14*r*r;
 		System.out.println("area is:"+area);
