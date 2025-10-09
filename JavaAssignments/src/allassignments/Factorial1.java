06058e80384ab888122caff699295b57766dfe67
Srija Ragula <srijareddy1808@gmail.com>
2025-10-09 14:00:04 +0530

    Update Factorial1.java

--- JavaAssignments/src/allassignments/Factorial1.java
+++ JavaAssignments/src/allassignments/Factorial1.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class Factorial1 {
 
-	public static void main(String[] args) {//j
+	public static void main(String[] args) {
 		int n=5; 
 		int fact=1;
 		for(int i=1;i<=5;i++) {
