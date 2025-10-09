3e01166052c4c52801746b7b87b7deb6c35a74cf
Srija Ragula <srijareddy1808@gmail.com>
2025-10-09 13:31:46 +0530

    Update CharAlphaFind.java

--- JavaAssignments/src/allassignments/CharAlphaFind.java
+++ JavaAssignments/src/allassignments/CharAlphaFind.java
@@ -1,7 +1,7 @@
 package allassignments;
 import java.util.*;
 
-public class CharAlphaFind {//j
+public class CharAlphaFind {//
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
