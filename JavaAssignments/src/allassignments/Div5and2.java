121c8db49dd4ef4421173e621ab0d3f6ec8ca168
Srija Ragula <srijareddy1808@gmail.com>
2025-10-11 11:33:41 +0530

    Update Div5and2.java

--- JavaAssignments/src/allassignments/Div5and2.java
+++ JavaAssignments/src/allassignments/Div5and2.java
@@ -7,7 +7,7 @@ public class Div5and2 {
 		if(s%5==0 && s%11==0) {
 			System.out.println("number divisibe by both 5 and 11");
 		}
-		else {//kj
+		else {
 			System.out.println("number is not divisible by 5 and 11");
 		}
 
