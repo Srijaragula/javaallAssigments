219478ca64914a7d4c920b479b8b44009e277a8f
Srija Ragula <srijareddy1808@gmail.com>
2025-10-11 11:33:07 +0530

    Update ColoursArray.java

--- JavaAssignments/src/allassignments/ColoursArray.java
+++ JavaAssignments/src/allassignments/ColoursArray.java
@@ -5,7 +5,7 @@ public class ColoursArray {
 	public static void main(String[] args) {   
 		String[] arr= {"pink","blue","black","violet"};
 		for(int i=0;i<arr.length;i++) {
-			System.out.println(arr[i]);
+			System.out.println(arr[i]); 
 		}
 
 	}
