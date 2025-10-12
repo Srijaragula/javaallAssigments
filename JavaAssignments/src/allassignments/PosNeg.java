e6108a8c68e651b902daed674df9fa33cc306ba5
Srija Ragula <srijareddy1808@gmail.com>
2025-10-12 11:39:33 +0530

    Update PosNeg.java

--- JavaAssignments/src/allassignments/PosNeg.java
+++ JavaAssignments/src/allassignments/PosNeg.java
@@ -4,7 +4,7 @@ public class PosNeg {
 
 	public static void main(String[] args) {
 		int a=-98;
-		String res=(a>=0)?"positive":"negative";//hb
+		String res=(a>=0)?"positive":"negative";
 		System.out.println(res);
 
 	}
