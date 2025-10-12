152ab07fc82ccb2eec3e6b2495ec08d618e8bba2
Srija Ragula <srijareddy1808@gmail.com>
2025-10-12 11:39:51 +0530

    Update PrimeNumber.java

--- JavaAssignments/src/allassignments/PrimeNumber.java
+++ JavaAssignments/src/allassignments/PrimeNumber.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class PrimeNumber {
 
-	public static void main(String[] args) {//jn
+	public static void main(String[] args) {
 		int num=7,count=0;
 		for(int i =1;i<=num;i++) {
 			if(num%i==0)
