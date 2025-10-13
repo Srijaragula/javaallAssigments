78782dab6e17612df8ff9159fe7094c483402008
Srija Ragula <srijareddy1808@gmail.com>
2025-10-13 10:59:59 +0530

    Update YoungestAge.java

--- JavaAssignments/src/allassignments/YoungestAge.java
+++ JavaAssignments/src/allassignments/YoungestAge.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class YoungestAge {
 
-	public static void main(String[] args) {/k
+	public static void main(String[] args) {
 		int age1=20;
 		int age2=10;
 		int age3=3;
@@ -10,7 +10,7 @@ public class YoungestAge {
 		
 		if(age1<age2 && age1<age3) {
 			youngest=age1;
-		}	else if(age2<age1 && age2<age3) {//bnjj
+		}	else if(age2<age1 && age2<age3) {
 			youngest=age2;
 		}		else {
 			youngest=age3;
