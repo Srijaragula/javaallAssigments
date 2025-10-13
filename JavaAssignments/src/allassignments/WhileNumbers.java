036bfedb9b31393187d68a1b1e18960da28e9ec1
Srija Ragula <srijareddy1808@gmail.com>
2025-10-13 10:59:38 +0530

    Update WhileNumbers.java

--- JavaAssignments/src/allassignments/WhileNumbers.java
+++ JavaAssignments/src/allassignments/WhileNumbers.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class WhileNumbers {
 
-	public static void main(String[] args) {/k
+	public static void main(String[] args) {
 		int num=0;
 		int i=0;
-		Scanner sc=new Scanner(System.in);//jhvhg
+		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the numbers");
 		num=sc.nextInt();
 		System.out.println("num value is:"+num);
