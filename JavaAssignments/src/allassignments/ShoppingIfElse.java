25da808fd73245c1eeb5dfed01976fb18b860e63
Srija Ragula <srijareddy1808@gmail.com>
2025-10-14 16:17:24 +0530

    Update ShoppingIfElse.java

--- JavaAssignments/src/allassignments/ShoppingIfElse.java
+++ JavaAssignments/src/allassignments/ShoppingIfElse.java
@@ -4,8 +4,8 @@ public class ShoppingIfElse {
 
 	public static void main(String[] args) {
 		int total=9000;
-		if(total>=10000) {
-			total-=1000;//kn
+		if(total>=10000) { 
+			total-=1000;
 		}
 		else 
 			System.out.println("no discount");
