a8f38b538363135ca58ddbc3141d5619d6afc602
Srija Ragula <srijareddy1808@gmail.com>
2025-10-14 16:03:00 +0530

    Update Num1.java

--- JavaAssignments/src/allassignments/Num1.java
+++ JavaAssignments/src/allassignments/Num1.java
@@ -1,12 +1,12 @@
 package allassignments;
 
-public class Num1 {
+public class Num1 { 
 
 	public static void main(String[] args) { 
 		int x = 10;   
 	       int y = 20;   
 	       int z = x++ + ++y;   
-	       System.out.println(z); //jnjn
+	       System.out.println(z); 
 		
 	}
 
