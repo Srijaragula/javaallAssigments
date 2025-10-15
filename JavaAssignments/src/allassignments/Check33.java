816d276a93d9192d2cf71f44c55d234bd9d637b1
Srija Ragula <srijareddy1808@gmail.com>
2025-10-15 10:35:32 +0530

    Update Check33.java

--- JavaAssignments/src/allassignments/Check33.java
+++ JavaAssignments/src/allassignments/Check33.java
@@ -6,7 +6,7 @@ public class Check33 {
 		int a=55;
 		int b=70;
 		boolean condition1=a<55;
-		boolean condition2=a<b;
+		boolean condition2=a<b; 
 		boolean bothconditiontrue=condition1 && condition2;
 		System.out.println(bothconditiontrue); 
 
