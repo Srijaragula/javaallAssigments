310ee09d7a331a6fec2e65fd7376c230f8aaae1f
Srija Ragula <srijareddy1808@gmail.com>
2025-10-15 10:40:49 +0530

    Update First1.java

--- JavaAssignments/src/allassignments/First1.java
+++ JavaAssignments/src/allassignments/First1.java
@@ -2,8 +2,8 @@ package allassignments;
 
 public class First1 { 
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");//hell
+	public static void main(String[] args) { 
+		System.out.println("Hello World");
 	}
 
 }
