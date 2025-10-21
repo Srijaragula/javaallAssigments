3cbcb42bc0d9593fa64fa85d60fb6b753d0c4c30
Srija Ragula <srijareddy1808@gmail.com>
2025-10-21 19:32:09 +0530

    Refactor Check33.java for clarity and formatting
    Fix formatting and update variable initialization.

--- JavaAssignments/src/allassignments/Check33.java
+++ JavaAssignments/src/allassignments/Check33.java
@@ -3,7 +3,7 @@ package allassignments;
 public class Check33 {
  
 	public static void main(String[] args) {   
-		int a=55;
+		int a=55; 
 		int b=70;
 		boolean condition1=a<55;
 		boolean condition2=a<b; 
