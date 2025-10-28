c36130c5a51aebae333c8eb36339858707daf889
Srija Ragula <srijareddy1808@gmail.com>
2025-10-28 15:32:46 +0530

    Update BankLoanManagementSystem.java

--- JavaAssignments/src/allassignments/BankLoanManagementSystem.java
+++ JavaAssignments/src/allassignments/BankLoanManagementSystem.java
@@ -1,7 +1,6 @@
 package allassignments;
 import java.util.Scanner; 
 public class BankLoanManagementSystem {   
-
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		
