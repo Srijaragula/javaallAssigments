c58c8bc9bb196d518cfb56335db40c78c5e5b2e2
Srija Ragula <srijareddy1808@gmail.com>
2025-11-09 22:52:42 +0530

    Update PerfectNumber.java

--- JavaAssignments/src/allassignments/PerfectNumber.java
+++ JavaAssignments/src/allassignments/PerfectNumber.java
@@ -3,7 +3,7 @@ package allassignments;
 public class PerfectNumber {
 	public static void main(String args[]) { 
 	int num=9;//jn
-	int sum=0;
+	int sum=0; 
 	//perfect number
 	//sum of its multiples equal to its actual numbers 
 	
