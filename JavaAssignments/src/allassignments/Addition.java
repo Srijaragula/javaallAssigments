74362ba40e92638eab2334bc0dd391f448372581
Srija Ragula <srijareddy1808@gmail.com>
2025-11-11 11:16:48 +0530

    Update Addition.java

--- JavaAssignments/src/allassignments/Addition.java
+++ JavaAssignments/src/allassignments/Addition.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Addition {  
 	public static void main(String[] args) {  
 		Scanner sc=new Scanner(System.in);   
-		int a,b,c;
+		int a,b,c; 
 		System.out.println("enter two numbers");
 		a=sc.nextInt();
 		b=sc.nextInt(); 
