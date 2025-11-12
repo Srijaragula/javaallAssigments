5dbf2e6663ba8fa014f79d5c7f8af034096b88a4
Srija Ragula <srijareddy1808@gmail.com>
2025-11-12 11:27:47 +0530

    Update CharAlphaFind.java

--- JavaAssignments/src/allassignments/CharAlphaFind.java
+++ JavaAssignments/src/allassignments/CharAlphaFind.java
@@ -7,7 +7,7 @@ public class CharAlphaFind {
 		String input=sc.next();  
 		char ch=input.CharAt(0);
 		if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z')) {
-			System.out.println("it is an alphabet");
+			System.out.println("it is an alphabet"); 
 		}
 		else if(ch>='0' && ch<='9') {
 			System.out.println("it is a digit");
