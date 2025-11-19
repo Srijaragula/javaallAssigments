5dccf408b5e1b196b9ff2bc95e6ba2b76d091f45
Srija Ragula <srijareddy1808@gmail.com>
2025-11-19 21:22:25 +0530

    Update IfElseIfLadder.java

--- JavaAssignments/src/allassignments/IfElseIfLadder.java
+++ JavaAssignments/src/allassignments/IfElseIfLadder.java
@@ -6,7 +6,7 @@ public class IfElseIfLadder {
 		int units =120;
 		float price=0.0f;
 		if(units>=10 && units<=100)
-			price=units*1.0f;//hgf
+			price=units*1.0f;
 		else if(units>100 && units<=200)
 			price=units*2.0f;
 		else if(units>200 && units<=300)//jhj
