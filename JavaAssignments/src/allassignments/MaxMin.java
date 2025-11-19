9d64cf9d90c8d58701eec347395e1427459730db
Srija Ragula <srijareddy1808@gmail.com>
2025-11-19 21:23:24 +0530

    Update MaxMin.java

--- JavaAssignments/src/allassignments/MaxMin.java
+++ JavaAssignments/src/allassignments/MaxMin.java
@@ -6,7 +6,7 @@ public class MaxMin {
   
 	public static void main(String[] args) { 
 	Scanner sc=new Scanner(System.in);
-	int a,b;
+	int a,b; 
 	System.out.println("Enter two numbers");
 	a=sc.nextInt();
 	b=sc.nextInt();
