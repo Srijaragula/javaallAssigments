3fa79ae6db59e480c8edffc9b35f0a0628b4b0e6
Srija Ragula <srijareddy1808@gmail.com>
2025-11-19 21:24:07 +0530

    Update NestedTernary.java

--- JavaAssignments/src/allassignments/NestedTernary.java
+++ JavaAssignments/src/allassignments/NestedTernary.java
@@ -4,7 +4,7 @@ public class NestedTernary {
 
 	public static void main(String[] args) { 
 		int a=20,b=15,c=50;
-		int result=(a>b)?((a>c)?a:c):((b>c)?b:c);
+		int result=(a>b)?((a>c)?a:c):((b>c)?b:c); 
 		System.out.println(result); 
 	}
 
