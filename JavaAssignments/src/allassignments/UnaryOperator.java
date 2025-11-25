f726ade9a9d76a10e0fe1f681046f050fd895699
Srija Ragula <srijareddy1808@gmail.com>
2025-11-25 11:03:20 +0530

    Update UnaryOperator.java

--- JavaAssignments/src/allassignments/UnaryOperator.java
+++ JavaAssignments/src/allassignments/UnaryOperator.java
@@ -2,7 +2,7 @@ package allassignments;
   
 public class UnaryOperator { 
   
-	public static void main(String[] args) { 
+	public static void main(String[] args) {  
 		int a=6,b=0,c=0,d=0;
 		b=a++ + --a;
 		System.out.println("A value is"+a);
