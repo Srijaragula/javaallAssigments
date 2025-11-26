b6f05e0da952fd94f353c13a3dd5b00da841238d
Srija Ragula <srijareddy1808@gmail.com>
2025-11-26 11:24:59 +0530

    Update WeekDay.java

--- JavaAssignments/src/allassignments/WeekDay.java
+++ JavaAssignments/src/allassignments/WeekDay.java
@@ -1,7 +1,7 @@
 package allassignments; 
 import java.util.*;
  
-public class WeekDay {   
+public class WeekDay {    
  
 	public static void main(String[] args) { 
 		Scanner sc=new Scanner(System.in);
