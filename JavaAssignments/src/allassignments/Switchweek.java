0a8f2de09fad539f9e4ed042a827aa6fbcbd7156
Srija Ragula <srijareddy1808@gmail.com>
2025-11-29 16:55:33 +0530

    Update Switchweek.java

--- JavaAssignments/src/allassignments/Switchweek.java
+++ JavaAssignments/src/allassignments/Switchweek.java
@@ -9,7 +9,7 @@ public class Switchweek {
 		case 2->"Tuesday";
 		case 3->"wednesday"; 
 		case 4->"Thursday";
-		case 5->"Friday";
+		case 5->"Friday"; 
 		case 6->"saturday";
 		case 7->"Sunday";
 		default->"Please Enter proper Input value";
