73a07bd561dc728ee16e89bc6be6bc3c63d497ca
Srija Ragula <srijareddy1808@gmail.com>
2025-12-01 10:46:48 +0530

    Update WhileNumbers.java

--- JavaAssignments/src/allassignments/WhileNumbers.java
+++ JavaAssignments/src/allassignments/WhileNumbers.java
@@ -7,7 +7,7 @@ public class WhileNumbers {
 		int num=0;    
 		int i=0;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the numbers");
+		System.out.println("enter the numbers"); 
 		num=sc.nextInt();
 		System.out.println("num value is:"+num);
 		while(i<=num) {
