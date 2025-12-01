a933dd217b1dd082270286d132eaf5c89ff8491a
Srija Ragula <srijareddy1808@gmail.com>
2025-12-01 10:44:39 +0530

    Update TernaryOperator.java

--- JavaAssignments/src/allassignments/TernaryOperator.java
+++ JavaAssignments/src/allassignments/TernaryOperator.java
@@ -3,7 +3,7 @@ package allassignments;
 public class TernaryOperator {
       
 	public static void main(String[] args) { 
-		int a=10,b=50; 
+		int a=10,b=50;  
 		int val=(a>b)?a:b; 
 		System.out.println(val); 
 
