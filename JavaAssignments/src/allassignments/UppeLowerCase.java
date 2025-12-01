fc74bdc4cdadb15dabc36068136160bc35817610
Srija Ragula <srijareddy1808@gmail.com>
2025-12-01 10:45:05 +0530

    Update UppeLowerCase.java

--- JavaAssignments/src/allassignments/UppeLowerCase.java
+++ JavaAssignments/src/allassignments/UppeLowerCase.java
@@ -2,7 +2,7 @@
 import java.util.*;   
   
 public class UppeLowerCase {  
-
+ 
 		    public static void main(String[] args) { 
 		        Scanner sc = new Scanner(System.in);   
 		        System.out.print("Enter a single character: ");
