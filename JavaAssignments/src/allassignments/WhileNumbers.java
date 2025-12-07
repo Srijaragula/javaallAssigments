d84189e21cfdd32daf2d7ef02a7a252c785cc0d3
Srija Ragula <srijareddy1808@gmail.com>
2025-12-07 11:55:32 +0530

    Update WhileNumbers.java

--- JavaAssignments/src/allassignments/WhileNumbers.java
+++ JavaAssignments/src/allassignments/WhileNumbers.java
@@ -1,7 +1,7 @@
 package allassignments; 
 import java.util.Scanner;  
  
-public class WhileNumbers { 
+public class WhileNumbers {  
  
 	public static void main(String[] args) {   
 		int num=0;    
