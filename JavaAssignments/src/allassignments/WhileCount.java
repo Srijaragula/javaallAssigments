9cb612569b6af56b5de8e1c3568ab85c9cc8e480
Srija Ragula <srijareddy1808@gmail.com>
2025-12-07 11:55:22 +0530

    Update WhileCount.java

--- JavaAssignments/src/allassignments/WhileCount.java
+++ JavaAssignments/src/allassignments/WhileCount.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class WhileCount {  
     
-	public static void main(String[] args) {    
+	public static void main(String[] args) {     
 		int n=234788959;   
 		int count=0;
 		while(n>0) { 
