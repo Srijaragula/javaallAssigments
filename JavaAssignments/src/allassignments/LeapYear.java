ebccb78efcf962fa9f2418c6dbd9b97d442a1617
Srija Ragula <srijareddy1808@gmail.com>
2025-12-08 10:28:28 +0530

    Update LeapYear.java

--- JavaAssignments/src/allassignments/LeapYear.java
+++ JavaAssignments/src/allassignments/LeapYear.java
@@ -1,7 +1,7 @@
 package allassignments;
 import java.util.*;
 
-public class LeapYear {
+public class LeapYear { 
  
 	public static void main(String[] args) { 
 		Scanner sc=new Scanner(System.in);
