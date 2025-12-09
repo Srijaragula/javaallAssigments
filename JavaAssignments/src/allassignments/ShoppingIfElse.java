1f8508e155f2339e804678659457c42dfe4a5309
Srija Ragula <srijareddy1808@gmail.com>
2025-12-09 13:19:51 +0530

    Update ShoppingIfElse.java

--- JavaAssignments/src/allassignments/ShoppingIfElse.java
+++ JavaAssignments/src/allassignments/ShoppingIfElse.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class ShoppingIfElse {  
  
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		int total=9000;
 		if(total>=10000) { 
 			total-=1000;
