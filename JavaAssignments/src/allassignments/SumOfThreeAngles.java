f27c8cff322f10b997dd2dc477cfc17f07e5d115
Srija Ragula <srijareddy1808@gmail.com>
2025-12-12 12:16:04 +0530

    Update SumOfThreeAngles.java

--- JavaAssignments/src/allassignments/SumOfThreeAngles.java
+++ JavaAssignments/src/allassignments/SumOfThreeAngles.java
@@ -2,7 +2,7 @@ package allassignments;
   
 public class SumOfThreeAngles {  
        
-	public static void main(String[] args) { 
+	public static void main(String[] args) {  
 		int ang1=60; 
 		int ang2=90;  
 		int ang3=75;  
