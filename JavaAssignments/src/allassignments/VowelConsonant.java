d90b2e71e26deaef79f14a9f0d1b4d18c412b27c
Srija Ragula <srijareddy1808@gmail.com>
2025-12-12 12:17:32 +0530

    Update VowelConsonant.java

--- JavaAssignments/src/allassignments/VowelConsonant.java
+++ JavaAssignments/src/allassignments/VowelConsonant.java
@@ -1,7 +1,7 @@
 package allassignments;
   
 public class VowelConsonant {     
-   
+    
 	public static void main(String[] args) { 
 		char ch='e';     
 		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {   
