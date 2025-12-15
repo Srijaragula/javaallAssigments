24f354b146d6bf809defab6088b35372315ad056
Srija Ragula <srijareddy1808@gmail.com>
2025-12-15 10:16:52 +0530

    Update Factorial1.java

--- JavaAssignments/src/allassignments/Factorial1.java
+++ JavaAssignments/src/allassignments/Factorial1.java
@@ -5,7 +5,7 @@ public class Factorial1 {
 	public static void main(String[] args) {   
 		int n=5; 
 		int fact=1;  
-		for(int i=1;i<=5;i++) { 
+		for(int i=1;i<=5;i++) {  
 			fact=fact*i;
 			
 			
