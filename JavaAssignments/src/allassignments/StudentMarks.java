b4c71f536f634587a398c8b3ba39461b0c7af862
Srija Ragula <srijareddy1808@gmail.com>
2025-12-16 14:22:36 +0530

    Update StudentMarks.java

--- JavaAssignments/src/allassignments/StudentMarks.java
+++ JavaAssignments/src/allassignments/StudentMarks.java
@@ -2,7 +2,7 @@ package allassignments;
 import java.util.*;  
   
 public class StudentMarks { 
- 
+  
 	public static void main(String[] args) {  
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter marks for 5 subjects:");
