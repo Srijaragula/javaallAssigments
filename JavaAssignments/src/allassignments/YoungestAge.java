36cf072a24ca0b355a20f099773e148f701f41c5
Srija Ragula <srijareddy1808@gmail.com>
2025-12-16 14:27:05 +0530

    Update YoungestAge.java

--- JavaAssignments/src/allassignments/YoungestAge.java
+++ JavaAssignments/src/allassignments/YoungestAge.java
@@ -5,7 +5,7 @@ public class YoungestAge {
 	public static void main(String[] args) {  
 		int age1=20;
 		int age2=10;  
-		int age3=3;  
+		int age3=3;   
 		int youngest;   
 		 
 		if(age1<age2 && age1<age3) {
