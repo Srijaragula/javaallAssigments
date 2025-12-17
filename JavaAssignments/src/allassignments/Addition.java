1690906e22c826a68c1449a922b893ca878cbf7f
Srija Ragula <srijareddy1808@gmail.com>
2025-12-17 14:27:04 +0530

    Update Addition.java

--- JavaAssignments/src/allassignments/Addition.java
+++ JavaAssignments/src/allassignments/Addition.java
@@ -1,5 +1,5 @@
 package allassignments;
-import java.util.*; 
+import java.util.*;  
 public class Addition {    
 	public static void main(String[] args) {   
 		Scanner sc=new Scanner(System.in);   
