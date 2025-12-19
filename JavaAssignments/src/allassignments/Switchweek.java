d57ca9c25e1884f368fdd55838cb55afae4e018e
Srija Ragula <srijareddy1808@gmail.com>
2025-12-19 11:17:59 +0530

    Update Switchweek.java

--- JavaAssignments/src/allassignments/Switchweek.java
+++ JavaAssignments/src/allassignments/Switchweek.java
@@ -4,7 +4,7 @@ public class Switchweek {
  
 	public static void main(String[] args) {    
 		int day =2;    
-		String result=switch(day) {  
+		String result=switch(day) {   
 		case 1->"Monday"; 
 		case 2->"Tuesday";
 		case 3->"wednesday"; 
