0a32be8103f96f831b5b3d75d2217695a4d7269e
Srija Ragula <srijareddy1808@gmail.com>
2025-12-23 15:46:40 +0530

    Update TernaryGrades.java

--- JavaAssignments/src/allassignments/TernaryGrades.java
+++ JavaAssignments/src/allassignments/TernaryGrades.java
@@ -5,7 +5,7 @@ public class TernaryGrades {
 	public static void main(String[] args) {  
 		int marks = 85;   
 		String result=(marks>90 && marks<=80)?"distinction":(marks>75)?"first class":(marks>50)?"second class":(marks>45)?"third class":"fail";
-		System.out.println(result);  
+		System.out.println(result);   
  
 	}
 
