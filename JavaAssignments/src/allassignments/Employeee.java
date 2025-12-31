4b1ff626972e8dcd9a4d1501ee4389bde5469dbc
Srija Ragula <srijareddy1808@gmail.com>
2025-12-31 11:58:51 +0530

    Update Employeee.java

--- JavaAssignments/src/allassignments/Employeee.java
+++ JavaAssignments/src/allassignments/Employeee.java
@@ -5,7 +5,7 @@ public class Employeee {
 	//instance variables   
 	int empid=180; 
 	String empname="srija"; 
-	//predefined static method//srija
+	//predefined static method 
 	public static void main(String args[]) { 
 		//use inside the ststic methd
 		//object creation
