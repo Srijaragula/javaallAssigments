f6e240c28c043222bcb8ca94612e0381d2f348d3
Srija Ragula <srijareddy1808@gmail.com>
2026-01-05 15:22:02 +0530

    Update EvenOdd.java

--- JavaAssignments/src/allassignments/EvenOdd.java
+++ JavaAssignments/src/allassignments/EvenOdd.java
@@ -3,7 +3,7 @@ package allassignments;
 public class EvenOdd {
  
 	public static void main(String[] args) {    
-		int a=6;  
+		int a=6;   
 		if(a%2==0){ 
 			System.out.println("even");//even
 			
