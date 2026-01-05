b2e46b9c6ad281e97994a00452499ee663ebe1bc
Srija Ragula <srijareddy1808@gmail.com>
2026-01-05 15:21:27 +0530

    Fix formatting in Employee.java main method

--- JavaAssignments/src/allassignments/Employee.java
+++ JavaAssignments/src/allassignments/Employee.java
@@ -3,7 +3,7 @@ package allassignments;
 public class Employee {    
    
 	static String companyname="LTTS";//l&t     
-	public static void main(String[] args) { 
+	public static void main(String[] args) {  
 		System.out.println("companyname "+companyname);
 		System.out.println("name: "+Employee.companyname);
 	}
