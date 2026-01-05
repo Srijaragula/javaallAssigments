20512cc2ddb49c5679549c8c97733effe7184e80
Srija Ragula <srijareddy1808@gmail.com>
2026-01-05 15:20:59 +0530

    Fix loop syntax for printing array elements

--- JavaAssignments/src/allassignments/ElementAtFirst.java
+++ JavaAssignments/src/allassignments/ElementAtFirst.java
@@ -5,7 +5,7 @@ public class ElementAtFirst {
 	public static void main(String[] args) { 
 		int[] r= {23,25,68,49,50};   
 		r[0]=18;  
-		for(int i=0;i<r.length;i++) { 
+		for(int i=0;i<r.length;i++) {  
 			System.out.println(r[i]);
 		}
 
