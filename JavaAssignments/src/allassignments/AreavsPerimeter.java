c44b506f48d155ab4d17e5bef0bdcd27924ad129
Srija Ragula <srijareddy1808@gmail.com>
2026-01-07 19:33:22 +0530

    Fix formatting issues in AreavsPerimeter.java

--- JavaAssignments/src/allassignments/AreavsPerimeter.java
+++ JavaAssignments/src/allassignments/AreavsPerimeter.java
@@ -2,7 +2,7 @@ package allassignments;
 public class AreavsPerimeter {    
 	public static void main(String[] args) {    
 		int length=20;    
-		int breadth=30;  
+		int breadth=30;   
 		int area=length*breadth;  
 		int perimeter=2*(length+breadth); 
 		System.out.println("Area:" +area);
