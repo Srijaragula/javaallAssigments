4aa7c8eb0568638061e7830bb6db30dc78b32c85
Srija Ragula <srijareddy1808@gmail.com>
2026-01-19 15:49:41 +0530

    Update Assign2.java

--- JavaAssignments/src/allassignments/Assign2.java
+++ JavaAssignments/src/allassignments/Assign2.java
@@ -2,7 +2,7 @@ package allassignments;
 public class Assign2 {     
 	public static void main(String[] args) {   
 		int a=2345;    
-		int b=a+8;
+		int b=a+8; 
 		int c=b/3;   
 		int d=c%5;  
 		int e=d*5;
