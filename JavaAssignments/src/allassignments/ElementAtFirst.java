701a4d5f16bdd261f25498f1f74a8a2e38fd75ef
Srija Ragula <srijareddy1808@gmail.com>
2026-01-24 10:37:37 +0530

    Update ElementAtFirst.java

--- JavaAssignments/src/allassignments/ElementAtFirst.java
+++ JavaAssignments/src/allassignments/ElementAtFirst.java
@@ -2,7 +2,7 @@ package allassignments;
 
 public class ElementAtFirst {   
     
-	public static void main(String[] args) { 
+	public static void main(String[] args) {  
 		int[] r= {23,25,68,49,50};    
 		r[0]=18;  
 		for(int i=0;i<r.length;i++) {  
