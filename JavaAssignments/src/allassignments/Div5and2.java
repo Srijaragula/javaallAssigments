3a15c9ff2667fa7070e3948fb91f1c03c90184a0
Srija Ragula <srijareddy1808@gmail.com>
2026-01-24 10:35:08 +0530

    Update Div5and2.java

--- JavaAssignments/src/allassignments/Div5and2.java
+++ JavaAssignments/src/allassignments/Div5and2.java
@@ -3,7 +3,7 @@ package allassignments;
 public class Div5and2 {  
  
 	public static void main(String[] args) {    
-		int s=8;      
+		int s=8;       
 		if(s%5==0 && s%11==0) {  
 			System.out.println("number divisibe by both 5 and 11");
 		}
