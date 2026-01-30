2ce2f81cace93d7a9785924efda143d39f8feb8d
Srija Ragula <srijareddy1808@gmail.com>
2026-01-30 10:39:43 +0530

    Update Employee.java

--- JavaAssignments/src/allassignments/Employee.java
+++ JavaAssignments/src/allassignments/Employee.java
@@ -6,5 +6,5 @@ public class Employee {
 	public static void main(String[] args) {  
 		System.out.println("companyname "+companyname);
 		System.out.println("name: "+Employee.companyname);
-	}
+	 }
 }
