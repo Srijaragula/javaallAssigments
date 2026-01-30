44c40299fff2014bf4d27c3f088eb86e8f0bdddd
Srija Ragula <srijareddy1808@gmail.com>
2026-01-30 10:42:07 +0530

    Update IfCondition.java

--- JavaAssignments/src/allassignments/IfCondition.java
+++ JavaAssignments/src/allassignments/IfCondition.java
@@ -3,7 +3,7 @@ package allassignments;
 public class IfCondition {  
  
 	public static void main(String[] args) {
-		int withdraw_amt=6000,bal_amount=7000;//jh
+		int withdraw_amt=6000,bal_amount=7000;
 		if(bal_amount>=withdraw_amt) 
 		{
 			bal_amount-=withdraw_amt;
