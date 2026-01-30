a85150d95fad8ddcdf1f5caeaabaeaa906e8a1c8
Srija Ragula <srijareddy1808@gmail.com>
2026-01-30 10:43:32 +0530

    Update MonthSwitch.java

--- JavaAssignments/src/allassignments/MonthSwitch.java
+++ JavaAssignments/src/allassignments/MonthSwitch.java
@@ -3,7 +3,7 @@ import java.util.*;
      
 public class MonthSwitch { 
  
-	public static void main(String[] args) { 
+	public static void main(String[] args) {  
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter months from 1-12");
 		int m=sc.nextInt();
