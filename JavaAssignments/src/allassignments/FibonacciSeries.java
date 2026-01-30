31980ff99124a2e881084732dda21ab20bdb48e7
Srija Ragula <srijareddy1808@gmail.com>
2026-01-30 10:40:48 +0530

    Update FibonacciSeries.java

--- JavaAssignments/src/allassignments/FibonacciSeries.java
+++ JavaAssignments/src/allassignments/FibonacciSeries.java
@@ -3,7 +3,7 @@ package allassignments;
 public class FibonacciSeries {   
      
 	public static void main(String[] args) {
-		//Addition of last two numbers is  the next number
+		//Addition of last two numbers is  the next number 
 		//0 1 1 2 3 5 8 
 		int n1=0,n2=1,n3=0;//high
 		System.out.print(n1+" "+n2+" ");//jnhj
