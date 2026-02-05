369754f2b13968f2670be915bd7ef860225d8550
Srija Ragula <srijareddy1808@gmail.com>
2026-02-05 10:43:41 +0530

    Fix formatting in AreaOfCircle.java

--- JavaAssignments/src/allassignments/AreaOfCircle.java
+++ JavaAssignments/src/allassignments/AreaOfCircle.java
@@ -1,6 +1,6 @@
 package allassignments;
 public class AreaOfCircle {  
-	public static void main(String[] args) {    
+	public static void main(String[] args) {     
 		int r=4;      
 		double area=3.14*r*r;     
 		System.out.println("area is:"+area);
