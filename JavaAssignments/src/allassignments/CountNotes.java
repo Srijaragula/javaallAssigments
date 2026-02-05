9a3cd9635e5ffe3df1a8b7c00bef6206ac4850d2
Srija Ragula <srijareddy1808@gmail.com>
2026-02-05 10:47:09 +0530

    Fix formatting in CountNotes.java

--- JavaAssignments/src/allassignments/CountNotes.java
+++ JavaAssignments/src/allassignments/CountNotes.java
@@ -3,7 +3,7 @@ import java.util.*;
      
 public class CountNotes {     
    
-	public static void main(String[] args) {   
+	public static void main(String[] args) {    
 	Scanner sc = new Scanner(System.in);  
     System.out.print("Enter amount: "); 
     int amount = sc.nextInt(); 
