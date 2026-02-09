3ab40a4e8fa734bae4ef09536f7301703c73f8a1
Srija Ragula <srijareddy1808@gmail.com>
2026-02-09 09:51:59 +0530

    Fix formatting issue in ElementsArray.java

--- JavaAssignments/src/allassignments/ElementsArray.java
+++ JavaAssignments/src/allassignments/ElementsArray.java
@@ -5,7 +5,7 @@ public class ElementsArray {
 	public static void main(String[] args) {    
 		int[] r= {23,25,68,49,50};  
 		for(int i=0;i<r.length;i++) { 
-			System.out.println(r[i]); 
+			System.out.println(r[i]);  
 		}
 		
 	}
