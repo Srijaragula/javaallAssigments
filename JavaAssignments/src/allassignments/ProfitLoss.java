3cd299555b77069684412826d3fbf0eb0cb1dc95
Srija Ragula <srijareddy1808@gmail.com>
2026-02-09 10:00:40 +0530

    Update ProfitLoss.java

--- JavaAssignments/src/allassignments/ProfitLoss.java
+++ JavaAssignments/src/allassignments/ProfitLoss.java
@@ -1,7 +1,7 @@
 package allassignments;
 import java.util.*;
 
-public class ProfitLoss {  
+public class ProfitLoss {   
   
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
