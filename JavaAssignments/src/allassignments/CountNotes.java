1d98912536a470f9efed79c185b2f95ec9444499
Srija Ragula <srijareddy1808@gmail.com>
2026-02-17 11:22:45 +0530

    Update CountNotes.java

--- JavaAssignments/src/allassignments/CountNotes.java
+++ JavaAssignments/src/allassignments/CountNotes.java
@@ -6,7 +6,7 @@ public class CountNotes {
 	public static void main(String[] args) {     
 	Scanner sc = new Scanner(System.in);  
     System.out.print("Enter amount: "); 
-    int amount = sc.nextInt(); 
+    int amount = sc.nextInt();  
     sc.close(); 
 
     int[] notes = {2000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
