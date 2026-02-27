b7e616ccde14975840d2a0e376a7c188c6a977a4
Srija Ragula <srijareddy1808@gmail.com>
2026-02-27 18:06:37 +0530

    Update Addition.java

--- JavaAssignments/src/allassignments/Addition.java
+++ JavaAssignments/src/allassignments/Addition.java
@@ -2,7 +2,7 @@ package allassignments;
 import java.util.*;  
 public class Addition {      
 	public static void main(String[] args) {     
-		Scanner sc=new Scanner(System.in);    
+		Scanner sc=new Scanner(System.in);     
 		int a,b,c;  
 		System.out.println("enter two numbers");
 		a=sc.nextInt();
