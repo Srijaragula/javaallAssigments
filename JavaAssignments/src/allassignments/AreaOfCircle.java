b9660f2cec6a6e8178f65c4814109920dd457987
Srija Ragula <srijareddy1808@gmail.com>
2026-03-01 14:45:37 +0530

    Update AreaOfCircle.java

--- JavaAssignments/src/allassignments/AreaOfCircle.java
+++ JavaAssignments/src/allassignments/AreaOfCircle.java
@@ -3,7 +3,7 @@ public class AreaOfCircle {
 	public static void main(String[] args) {      
 		int r=4;       
 		double area=3.14*r*r;      
-		System.out.println("area is:"+area);
+		System.out.println("area is:"+area); 
 	}  
 } 
  
